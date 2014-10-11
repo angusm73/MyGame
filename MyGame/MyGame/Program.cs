90dd218b5a6f2feea684ea194717d83ad9eadb2c
Angus Moore <toxic0323@gmail.com>
2014-10-11 19:29:08 +1100

    Initial commit

---
+++ MyGame/MyGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace MyGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, this is where I will be making a C# game using OpenGL through OpenTK.");
+        }
+    }
+}
