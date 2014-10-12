bf0d894c38d3bf0030ce6f07db191d4ac9bf9509
Angus Moore <toxic0323@gmail.com>
2014-10-12 14:09:37 +1100

    Basic window setup Derived from OpenTK Simple GameWindow example.

--- MyGame/MyGame/Program.cs
+++ MyGame/MyGame/Program.cs
@@ -1,13 +1,156 @@
 ﻿using System;
+using System.Drawing;
 using System.IO;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using OpenTK.Platform;
 
 namespace MyGame
 {
-    class Program
+    /// <summary>
+    /// Demonstrates the GameWindow class.
+    /// </summary>
+    // [Example("GameWindow Simple", ExampleCategory.OpenTK, "GameWindow", 1, Documentation = "GameWindowSimple")]
+    // Not sure where the Example class comes from.
+    public class MyGameWindow : GameWindow
     {
-        static void Main(string[] args)
+        public MyGameWindow() : base(800, 600)
         {
-            Console.WriteLine("Hello, this is where I will be making a C# game using OpenGL through OpenTK.");
+            KeyDown += Keyboard_KeyDown;
         }
+
+        #region Keyboard_KeyDown
+
+        /// <summary>
+        /// Occurs when a key is pressed.
+        /// </summary>
+        /// <param name="sender">The KeyboardDevice which generated this event.</param>
+        /// <param name="e">The key that was pressed.</param>
+        void Keyboard_KeyDown(object sender, KeyboardKeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case (Key.Escape):
+                    this.Exit();
+                    break;
+                
+                case (Key.F11):
+                    if (this.WindowState == WindowState.Fullscreen)
+                        this.WindowState = WindowState.Normal;
+                    else
+                        this.WindowState = WindowState.Fullscreen;
+                    break;
+                
+                default:
+                    Console.WriteLine("I notice that a key has been pressed, but I am not sure how to handle it.");
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region OnLoad
+
+        /// <summary>
+        /// Setup OpenGL and load resources here.
+        /// </summary>
+        /// <param name="e">Not used.</param>
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(Color.MidnightBlue);
+        }
+
+        #endregion
+
+        #region OnResize
+
+        /// <summary>
+        /// Respond to resize events here.
+        /// </summary>
+        /// <param name="e">Contains information on the new GameWindow size.</param>
+        /// <remarks>There is no need to call the base implementation.</remarks>
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
+        }
+
+        #endregion
+
+        #region OnUpdateFrame
+
+        /// <summary>
+        /// Add your game logic here.
+        /// </summary>
+        /// <param name="e">Contains timing information.</param>
+        /// <remarks>There is no need to call the base implementation.</remarks>
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            // Nothing to do, yet.
+        }
+
+        #endregion
+
+        #region OnRenderFrame
+
+        /// <summary>
+        /// Add your game rendering code here.
+        /// </summary>
+        /// <param name="e">Contains timing information.</param>
+        /// <remarks>There is no need to call the base implementation.</remarks>
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            
+            // Shape 1 (Right Triangle)
+            GL.Begin(PrimitiveType.Triangles);
+
+            GL.Color3(Color.MidnightBlue);
+            GL.Vertex2(0.0f, 0.0f);
+            GL.Color3(Color.SpringGreen);
+            GL.Vertex2(1.0f, -1.0f);
+            GL.Color3(Color.Ivory);
+            GL.Vertex2(1.0f, 1.0f);
+
+            GL.End();
+
+            // Shape 2 (Left Triangle)
+            GL.Begin(PrimitiveType.Triangles);
+
+            GL.Color3(Color.MidnightBlue);
+            GL.Vertex2(0.0f, 0.0f);
+            GL.Color3(Color.Ivory);
+            GL.Vertex2(-1.0f, -1.0f);
+            GL.Color3(Color.SpringGreen);
+            GL.Vertex2(-1.0f, 1.0f);
+
+            GL.End();
+
+            this.SwapBuffers();
+        }
+
+        #endregion
+
+        #region public static void Main()
+
+        /// <summary>
+        /// Entry point of this example.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            using (MyGameWindow example = new MyGameWindow())
+            {
+                // Get the title and category  of this example using reflection.
+                // Utilities.SetWindowTitle(example);
+                example.Run(30.0, 0.0);
+            }
+        }
+
+        #endregion
     }
 }
