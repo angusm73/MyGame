e85205d96ae7588a8d5c08dcbad12ff290336b21
Angus Moore <toxic0323@gmail.com>
2014-10-12 15:11:53 +1100

    Adding the WriteLog.cs derp

---
+++ MyGame/MyGame/WriteLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+    class WriteLog
+    {
+        private String from;
+        private String message;
+
+        public WriteLog(String from, String message)
+        {
+            Console.WriteLine("[" + DateTime.Now.ToString("h:mm:ss") + "] [" + from + "] " + message);
+        }
+    }
+}
