45cdc857b95b4f60bfdb3c335450f8ff91421434
Angus Moore <toxic0323@gmail.com>
2014-10-13 16:49:42 +1100

    Implemented DrawShape class

--- MyGame/MyGame/DrawShape.cs
+++ MyGame/MyGame/DrawShape.cs
@@ -10,14 +10,40 @@ namespace MyGame
 {
     class DrawShape
     {
-        public void Sqaure(int x, int y, int sx, int sy, float alpha)
+        public void Square(float x, float y, float sx, float sy, float r, float g, float b, float alpha)
         {
-            
-        }
-        
-        public void Circle(int x, int y, int size, float alpha)
-        {
+            GL.Begin(PrimitiveType.Quads);
 
+            GL.Color4(r, g, b, alpha);
+            GL.Vertex2(x, y + sy);
+            GL.Vertex2(x, y);
+            GL.Vertex2(x + sx, y);
+            GL.Vertex2(x + sx, y + sy);
+
+            GL.End();
+        }
+
+        void Circle(float cx, float cy, float r, int num_segments)
+        {
+            double theta = 2 * 3.1415926 / Convert.ToDouble(num_segments);
+            double c = Math.Cos(theta);
+            double s = Math.Sin(theta);
+            double t;
+
+            double x = r;//we start at angle = 0 
+            double y = 0;
+
+            GL.Begin(PrimitiveType.LineLoop);
+            for (int ii = 0; ii < num_segments; ii++)
+            {
+                GL.Vertex2(x + cx, y + cy);//output vertex 
+
+                //apply the rotation matrix
+                t = x;
+                x = c * x - s * y;
+                y = s * t + c * y;
+            }
+            GL.End();
         }
     }
 }
