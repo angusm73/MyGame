2657bc886be1b24fdfa05a8d101f493e54c63da5
Angus Moore <toxic0323@gmail.com>
2014-10-17 17:25:13 +1100

    Added GOgameTerrain.cs Not implemented just yet, this is just going to be the ground, the ground will be generated in sections.

---
+++ MyGame/MyGame/GOgameTerrain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+    class GOgameTerrain : GameObject
+    {
+        public GOgameTerrain(float x, float y, float sx, float sy, float r, float g, float b, float a) : base(x, y, sx, sy, r, g, b, a)
+        {
+            // nothing to put here just yet
+        }
+
+        public override void update(double x, double y)
+        {
+            // these are static objects at the moment that dont need to be updated just yet
+        }
+
+        public override void render()
+        {
+            DrawShape draw = new DrawShape();
+            draw.Square(x, y, sx, sy, r, g, b, a);
+        }
+    }
+}
